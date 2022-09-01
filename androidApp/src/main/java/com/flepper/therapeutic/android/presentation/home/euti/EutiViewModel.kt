863bc693adda39abeb9530409cb543eb8fb50b08
Isaac <37780207+develNerd@users.noreply.github.com>
2022-09-01 18:37:36 +0000

    Updated Date for testing purposes for @SquareTeam

--- androidApp/src/main/java/com/flepper/therapeutic/android/presentation/home/euti/EutiViewModel.kt
+++ androidApp/src/main/java/com/flepper/therapeutic/android/presentation/home/euti/EutiViewModel.kt
@@ -333,8 +333,10 @@ class EutiViewModel : BaseViewModel() {
                     )
                     appPreferences.signInUser = signInResult
                 } else {
-                    _signUpResponse.value = OnResultObtained(signInResult, true)
-                    appPreferences.signInUser = signInResult
+                    _signUpResponse.value = OnResultObtained(signInResult.apply { squareCustomerID = result.customer_id }, true)
+                    appPreferences.signInUser = signInResult.apply {
+
+                    }
                 }
             },
             onError = {
